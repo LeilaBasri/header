675338bf4e27dfb6f39613adf54d180ac15f92a9
Leila Basri <leilabasrilb@gmail.com>
2024-01-03 22:43:56 -0800

    update

--- index.js
+++ index.js
@@ -1,164 +1,164 @@
-<script>
+
 function fixedNavigation(){
     $("#header-Container").load("templates/fixedNavigation.html");
 }   
-</script>
 
-<script>
+
+
     function iconsSidebar(){
         $("#header-Container").load("templates/iconsSidebar.html");
     }   
-</script>
 
-<script>
+
+
     function navbarIcons(){
         $("#header-Container").load("templates/navbar-Icons.html");  
     }
-</script>
 
-<script>
+
+
     function fullScreenOverlayNav(){
         $("#header-Container").load("templates/fullScreen-OverlayNav.html");
     }
-</script>
 
-<script>
+
+
     function overlayNavSlideDown(){
         $("#header-Container").load("templates/overlayNav-SlideDown.html");
     }
-</script>
+
  
-<script>
+
     function offCanvasMenu(){
         $("#header-Container").load("templates/off-Canvas-Menu.html");
     }
-</script>  
   
-<script>
+  
+
     function hoverableSidenavButtons(){
         $("#header-Container").load("templates/hoverable-SidenavButtons.html");
     }
-</script>
+
   
-<script>
+
     function bottomNavigation(){
         $("#header-Container").load("templates/bottom-Navigation.html");
     }
-</script>
 
-<script> 
+
+ 
     function bottomBorderNav(){
         $("#header-Container").load("templates/bottom-BorderNav.html");  
     }
-</script> 
+ 
   
-<script> 
+ 
     function rightAlignedMenu(){
         $("#header-Container").load("templates/right-Aligned-Menu.html");  
     }
-</script>
 
-<script> 
+
+ 
     function slideDownOnScroll(){
         $("#header-Container").load("templates/slideDown-OnScroll.html");  
     }
-</script>
+
   
-<script> 
+ 
     function hideNavbarOnScrollDown(){
         $("#header-Container").load("templates/hideNavbar-OnScrollDown.html");  
     }
-</script>
 
-<script> 
+
+ 
     function shrinkNavMenuOnScroll(){
         $("#header-Container").load("templates/shrinkNavMenu-OnScroll.html");  
     }
-</script>
 
-<script> 
+
+ 
     function stickyNavbar(){
         $("#header-Container").load("templates/stickyNavbar.html");  
     }
-</script>
 
-<script> 
+
+ 
     function navbarOnImage(){
         $("#header-Container").load("templates/navbarOnImage.html");  
     }
-</script>
 
-<script> 
+
+ 
     function hoverableDropdown(){
         $("#header-Container").load("templates/hoverableDropdown.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
 
-<script> 
+
+ 
     function s(){
         $("#header-Container").load("templates/s.html");  
     }
-</script>
+
 
 
 
